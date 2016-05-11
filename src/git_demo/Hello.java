b464cd5d608d087fb8c9b86aedbddf8f91e2347a
zwj123456 <wuxi.zwj@foxmail.com>
2016-05-11 14:12:27 +0800

    ddd

--- src/git_demo/Hello.java
+++ src/git_demo/Hello.java
@@ -2,7 +2,8 @@ package git_demo;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("bbb");
+		System.out.println("eee");
 		System.out.println(999);
+		System.out.println(555);
 	}
 }
