703236d20c0fcccf6bca3874954646c1443fdbf4
zwj123456 <wuxi.zwj@foxmail.com>
2016-05-11 14:13:34 +0800

    111

--- src/git_demo/Hello.java
+++ src/git_demo/Hello.java
@@ -2,7 +2,7 @@ package git_demo;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("ccc");
-		System.out.println("dfdf");
+		System.out.println("gfsfefs");
+		System.out.println("wefwe");
 	}
 }
