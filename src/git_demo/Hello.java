53e6507d05bbde43961334f401fd7536bdaf6a9d
zwj123456 <wuxi.zwj@foxmail.com>
2016-05-11 13:52:51 +0800

    jjj

--- src/git_demo/Hello.java
+++ src/git_demo/Hello.java
@@ -2,15 +2,9 @@ package git_demo;
 
 public class Hello {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("bbb");
-		System.out.println("ccc");
-		System.out.println("def");
-		System.out.println("ddd");
-		System.out.println(123);
+		System.out.println("dfdf");
 		System.out.println(33);
-=======
-		System.out.println("bbb");
->>>>>>> branch 'master' of https://github.com/zwj123456/git_demo.git
+		System.out.println(11);
 	}
 }
