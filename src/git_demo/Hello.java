5f2cf726b31cfc09d309e5d3ebe2600ad04f54de
zwj123456 <wuxi.zwj@foxmail.com>
2016-05-11 14:05:23 +0800

    请问

--- src/git_demo/Hello.java
+++ src/git_demo/Hello.java
@@ -3,9 +3,5 @@ package git_demo;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("bbb");
-		System.out.println("dfdf");
-		System.out.println(33);
-		System.out.println(11);
-		System.out.println(55);
 	}
 }
