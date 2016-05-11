8c13bd4abac8c46ec595e580411ca9b799b7fd17
zwj123456 <wuxi.zwj@foxmail.com>
2016-05-11 14:08:58 +0800

    生生世世

--- src/git_demo/Hello.java
+++ src/git_demo/Hello.java
@@ -2,11 +2,7 @@ package git_demo;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("bbb");
+		System.out.println("ccc");
 		System.out.println("dfdf");
-		System.out.println(33);
-		System.out.println(999);
-		System.out.println(11);
-		System.out.println(55);
 	}
 }
